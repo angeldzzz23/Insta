ad6616a59ca902e514b3fd4010693644bf206549
angel zambrano <angeldzambrano99@gmail.com>
2022-10-17 21:53:35 -0700

    added the addcomment lbl

--- Insta/Insta/AddCommentTableViewCell.swift
+++ Insta/Insta/AddCommentTableViewCell.swift
@@ -10,16 +10,35 @@ import UIKit
 class AddCommentTableViewCell: UITableViewCell {
      
     
-        static let identifier = "AddCommentTableViewCell"
+    static let identifier = "AddCommentTableViewCell"
     
+    private let lbl: UILabel = {
+       let lbl = UILabel()
+        lbl.text = "Add Comment"
+        lbl.textColor = .lightGray
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+    }()
     
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
             
-        contentView.backgroundColor = .yellow
+        addViews()
+        addConstraints()
     }
     
+    
+    private func addViews() {
+        contentView.addSubview(lbl)
+    }
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            lbl.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            lbl.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10)
+        ])
+    }
+
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
